2bb95235e618bce7770e24911211dc701de3edb2
Knightly <knightly.cavalier@gmail.com>
2021-09-10 22:30:18 -1000

    Clean up mixed tabs and spaces/voids

--- MQ2AutoCamp.cpp
+++ MQ2AutoCamp.cpp
@@ -4,10 +4,13 @@
 // v1.02 :: Sym - 2013-04-13 :: Added check for rez in same zone as bind point to cancel camping
 // v1.03 :: Sym - 2013-04-24 :: Added ini support, start/end macros on death/rez
 
-#include "../MQ2Plugin.h"
+#include <mq/Plugin.h>
+
+PreSetup("MQ2AutoCamp");
 PLUGIN_VERSION(1.03);
 
-#define SKIP_PULSES 100
+constexpr int SKIP_PULSES = 100;
+
 bool bAutoCampEnabled = false;
 bool bCamp = false;
 bool bEndMacro = false;
@@ -27,19 +30,19 @@ time_t StartTime;
 time_t WarningTime;
 time_t ResetTime;
 
-PreSetup("MQ2AutoCamp");
-
-void Update_INIFileName() {
-    //sprintf_s(INIFileName,"%s\\%s_%s.ini",gszINIPath, EQADDR_SERVERNAME, GetCharInfo()->Name);
-	sprintf_s(INIFileName,"%s\\MQ2AutoCamp.ini",gszINIPath);
+void Update_INIFileName()
+{
+	//sprintf_s(INIFileName,"%s\\%s_%s.ini",gszINIPath, EQADDR_SERVERNAME, GetCharInfo()->Name);
+	sprintf_s(INIFileName,"%s\\MQ2AutoCamp.ini", gPathConfig);
 }
 
-VOID SaveINI(VOID) {
-    Update_INIFileName();
-    // write on/off settings
+void SaveINI()
+{
+	Update_INIFileName();
+	// write on/off settings
 	char ToStr[16];
-    sprintf_s(szTemp,"%s_Settings",GetCharInfo()->Name);
-    WritePrivateProfileSection(szTemp, "", INIFileName);
+	sprintf_s(szTemp,"%s_Settings",GetCharInfo()->Name);
+	WritePrivateProfileSection(szTemp, "", INIFileName);
 	WritePrivateProfileString(szTemp,"Enabled",bAutoCampEnabled? "1" : "0",INIFileName);
 	sprintf_s(ToStr,"%d",CampCount);
 	WritePrivateProfileString(szTemp,"WaitMinutes",ToStr,INIFileName);
@@ -51,11 +54,11 @@ VOID SaveINI(VOID) {
 	WriteChatf("MQ2AutoCamp :: Settings updated");
 }
 
-VOID LoadINI(VOID) {
-
-    Update_INIFileName();
-    // get on/off settings
-    sprintf_s(szTemp,"%s_Settings",GetCharInfo()->Name);
+void LoadINI()
+{
+	Update_INIFileName();
+	// get on/off settings
+	sprintf_s(szTemp,"%s_Settings",GetCharInfo()->Name);
 	bAutoCampEnabled = GetPrivateProfileInt(szTemp, "Enabled", 0, INIFileName) > 0 ? true : false;
 	CampCount = GetPrivateProfileInt(szTemp, "WaitMinutes", 15, INIFileName);
 	bEndMacro = GetPrivateProfileInt(szTemp, "Endmac", 0, INIFileName) > 0 ? true : false;
@@ -70,14 +73,14 @@ VOID LoadINI(VOID) {
 	bInitDone = true;
 }
 
-
-void ShowHelp(void) {
+void ShowHelp()
+{
 	WriteChatf("\atMQ2AutoCamp :: v%1.2f :: by Sym for RedGuides.com\ax", MQ2Version);
 	WriteChatf("/autocamp :: Lists command syntax");
-    WriteChatf("/autocamp load :: Loads ini file.");
-    WriteChatf("/autocamp save :: Saves settings to ini. Changes \arDO NOT\ax auto save.");
+	WriteChatf("/autocamp load :: Loads ini file.");
+	WriteChatf("/autocamp save :: Saves settings to ini. Changes \arDO NOT\ax auto save.");
 	WriteChatf("/autocamp on|off :: Toggles camping to desktop when dead. Default is \arOFF\ax");
-	WriteChatf("/autocamp endmac on|off :: Toggles /endmac before camping. Default is \arOFF\ax");	
+	WriteChatf("/autocamp endmac on|off :: Toggles /endmac before camping. Default is \arOFF\ax");
 	WriteChatf("/autocamp endmac whendead on|off :: Toggles /endmac as soon as dead. Default is \arOFF\ax");
 	WriteChatf("/autocamp startmac on|off|clear|set macroname :: Toggles /mac macroname after rez. Default is \arOFF\ax");
 	WriteChatf("/autocamp abort :: Aborts processing the currently detected death.");
@@ -85,7 +88,9 @@ void ShowHelp(void) {
 	WriteChatf("/autocamp status :: Shows the current status.");
 
 }
-void ShowStatus(void) {
+
+void ShowStatus()
+{
 	WriteChatf("\atMQ2AutoCamp :: v%1.2f :: by Sym for RedGuides.com\ax", MQ2Version);
 	WriteChatf("MQ2AutoCamp :: %s", bAutoCampEnabled?"\agENABLED\ax":"\arDISABLED\ax");
 	WriteChatf("MQ2AutoCamp :: Waiting \ag%d minutes\ax before camping",CampCount);
@@ -94,91 +99,92 @@ void ShowStatus(void) {
 	if (bStartMacro) WriteChatf("MQ2AutoCamp :: Macro to start after rez is \ag%s\ax", szMacroName);
 }
 
-VOID AutoCampCommand(PSPAWNINFO pChar, PCHAR zLine) {
-    char szArg1[MAX_STRING],szArg2[MAX_STRING],szArg3[MAX_STRING];
+void AutoCampCommand(PSPAWNINFO pChar, PCHAR zLine)
+{
+	char szArg1[MAX_STRING],szArg2[MAX_STRING],szArg3[MAX_STRING];
 
-    GetArg(szArg1, zLine, 1);
+	GetArg(szArg1, zLine, 1);
 	GetArg(szArg2, zLine, 2);
 	GetArg(szArg3, zLine, 3);
 
-    if(!_strnicmp(szArg1,"load",4)) {
-        LoadINI();
+	if(!_strnicmp(szArg1,"load",4)) {
+		LoadINI();
 		WriteChatf("MQ2AutoCamp :: \agSettings loaded\ax");
 		ShowStatus();
-        return;
-    }
-    if(!_strnicmp(szArg1,"save",4)) {
-        SaveINI();
-        return;
-    }
-    if(!_strnicmp(szArg1,"on",2)) {
-        bAutoCampEnabled = true;
-        WriteChatf("MQ2AutoCamp :: \agEnabled\ax");
-    }
-    else if(!_strnicmp(szArg1,"off",2)) {
-        bAutoCampEnabled = false;
-        WriteChatf("MQ2AutoCamp :: \arDisabled\ax");
-    }
-    else if(!_strnicmp(szArg1,"abort",2)) {
-        bCamp = false;
+		return;
+	}
+	if(!_strnicmp(szArg1,"save",4)) {
+		SaveINI();
+		return;
+	}
+	if(!_strnicmp(szArg1,"on",2)) {
+		bAutoCampEnabled = true;
+		WriteChatf("MQ2AutoCamp :: \agEnabled\ax");
+	}
+	else if(!_strnicmp(szArg1,"off",2)) {
+		bAutoCampEnabled = false;
+		WriteChatf("MQ2AutoCamp :: \arDisabled\ax");
+	}
+	else if(!_strnicmp(szArg1,"abort",2)) {
+		bCamp = false;
 		ResetTime = time(NULL);
-        WriteChatf("MQ2AutoCamp :: Camp to desktop \arABORTED\ax");
-    }
-    else if (!_stricmp(szArg1, "status")) {
+		WriteChatf("MQ2AutoCamp :: Camp to desktop \arABORTED\ax");
+	}
+	else if (!_stricmp(szArg1, "status")) {
 		ShowStatus();
-    } else if(!_strcmpi(szArg1,"wait")) {
-        if(!_strcmpi(szArg2, "")) {
-            WriteChatf("Usage: /autocamp wait #");
-            WriteChatf("       Number of minutes to wait before camping to desktop.");
-            return;
-        } else {
-            CampCount = atoi(szArg2) > 0 ? atoi(szArg2) : 15 ;
-            WriteChatf("MQ2AutoCamp :: Camping to desktop %s minutes after death.",szArg2);
-        }
+	} else if(!_strcmpi(szArg1,"wait")) {
+		if(!_strcmpi(szArg2, "")) {
+			WriteChatf("Usage: /autocamp wait #");
+			WriteChatf("       Number of minutes to wait before camping to desktop.");
+			return;
+		} else {
+			CampCount = atoi(szArg2) > 0 ? atoi(szArg2) : 15 ;
+			WriteChatf("MQ2AutoCamp :: Camping to desktop %s minutes after death.",szArg2);
+		}
 	} else if(!_strcmpi(szArg1,"endmac")) {
-        if(!_strcmpi(szArg2, "")) {
-            WriteChatf("Usage: /autocamp endmac on|off");
+		if(!_strcmpi(szArg2, "")) {
+			WriteChatf("Usage: /autocamp endmac on|off");
 			WriteChatf("       Toggles /endmac before camping. Default is \arOFF\ax");
-            return;
-        }
-        if(!_strcmpi(szArg2, "on")) {
-            bEndMacro = true;
-        }
-        else if(!_strcmpi(szArg2, "off")) {
-            bEndMacro = false;
-        }
+			return;
+		}
+		if(!_strcmpi(szArg2, "on")) {
+			bEndMacro = true;
+		}
+		else if(!_strcmpi(szArg2, "off")) {
+			bEndMacro = false;
+		}
 		else if(!_strcmpi(szArg2, "whendead")) {
 			if(!_strcmpi(szArg3, "")) {
-	            WriteChatf("Usage: /autocamp endmac whendead on|off");
+				WriteChatf("Usage: /autocamp endmac whendead on|off");
 				WriteChatf("       Toggles /endmac as soon as death is detected. Default is \arOFF\ax");
 				return;
 			}
 			if(!_strcmpi(szArg3, "on")) {
-	            bEndMacroWhenDead = true;
+				bEndMacroWhenDead = true;
 			}
 			else if(!_strcmpi(szArg3, "off")) {
-	            bEndMacroWhenDead = false;
+				bEndMacroWhenDead = false;
 			}
 			WriteChatf("MQ2AutoCamp :: /endmac when dead is %s", bEndMacroWhenDead ?"\agON\ax":"\arOFF\ax");
 			return;
 		}
 		WriteChatf("MQ2AutoCamp :: /endmac before camping is %s", bEndMacro?"\agON\ax":"\arOFF\ax");
 	} else if(!_strcmpi(szArg1,"startmac")) {
-        if(!_strcmpi(szArg2, "")) {
-            WriteChatf("Usage: /autocamp startmac on|off");
+		if(!_strcmpi(szArg2, "")) {
+			WriteChatf("Usage: /autocamp startmac on|off");
 			WriteChatf("       Toggles /mac macroname after getting a rez. Default is \arOFF\ax");
-            return;
-        }
-        if(!_strcmpi(szArg2, "on")) {
-            bStartMacro = true;
-        }
-        else if(!_strcmpi(szArg2, "off")) {
-            bStartMacro = false;
-        }
+			return;
+		}
+		if(!_strcmpi(szArg2, "on")) {
+			bStartMacro = true;
+		}
+		else if(!_strcmpi(szArg2, "off")) {
+			bStartMacro = false;
+		}
 		else if(!_strcmpi(szArg2, "set")) {
 			sprintf_s(szMacroName,"%s",szArg3);
 			if(!_strcmpi(szArg3, "")) {
-	            WriteChatf("Usage: /autocamp startmac set \"macroname param1 param2 ... paramN\" ");
+				WriteChatf("Usage: /autocamp startmac set \"macroname param1 param2 ... paramN\" ");
 				WriteChatf("       Sets macroname to be started after getting a rez.");
 			} else {
 				WriteChatf("MQ2AutoCamp :: Macro to start after rez is \ag%s\ax", szMacroName);
@@ -191,7 +197,7 @@ VOID AutoCampCommand(PSPAWNINFO pChar, PCHAR zLine) {
 			return;
 		}
 		WriteChatf("MQ2AutoCamp :: Starting macro after rez is %s", bStartMacro?"\agON\ax":"\arOFF\ax");
-    } else {
+	} else {
 		ShowHelp();
 	}
 
@@ -200,33 +206,33 @@ VOID AutoCampCommand(PSPAWNINFO pChar, PCHAR zLine) {
 // Called once, when the plugin is to initialize
 PLUGIN_API VOID InitializePlugin(VOID)
 {
-    DebugSpewAlways("Initializing MQ2AutoCamp");
-    AddCommand("/autocamp",AutoCampCommand);
+	DebugSpewAlways("Initializing MQ2AutoCamp");
+	AddCommand("/autocamp",AutoCampCommand);
 }
 
 // Called once, when the plugin is to shutdown
 PLUGIN_API VOID ShutdownPlugin(VOID)
 {
-    DebugSpewAlways("Shutting down MQ2AutoCamp");
-    RemoveCommand("/autocamp");
+	DebugSpewAlways("Shutting down MQ2AutoCamp");
+	RemoveCommand("/autocamp");
 }
 
 
 // Called once directly after initialization, and then every time the gamestate changes
 PLUGIN_API VOID SetGameState(DWORD GameState)
 {
-    DebugSpewAlways("MQ2AutoCamp::SetGameState()");
-    if(GameState==GAMESTATE_INGAME) {
-        if (!bInitDone) {
+	DebugSpewAlways("MQ2AutoCamp::SetGameState()");
+	if(GameState==GAMESTATE_INGAME) {
+		if (!bInitDone) {
 			LoadINI();
 		}
-    } else if(GameState!=GAMESTATE_LOGGINGIN) {
-        if (bInitDone) bInitDone=false;
-    }
+	} else if(GameState!=GAMESTATE_LOGGINGIN) {
+		if (bInitDone) bInitDone=false;
+	}
 }
 
 
-void PopUpWarning(PCHAR szText) 
+void PopUpWarning(PCHAR szText)
 {
 	// show popup in                red                 for 30 seconds
 	((CTextOverlay*)pTextOverlay)->DisplayText(szText,13,10,255,100,500,30000);
@@ -237,8 +243,8 @@ PLUGIN_API VOID OnPulse(VOID)
 {
 	if (GetGameState() != GAMESTATE_INGAME || !bAutoCampEnabled) return;
 	SkipPulse++;
-    if (SkipPulse == SKIP_PULSES) {
-        SkipPulse = 0;
+	if (SkipPulse == SKIP_PULSES) {
+		SkipPulse = 0;
 		// death hasn't been detected yet
 		if (!bCamp) {
 			// do we have revival sickness?
@@ -276,7 +282,7 @@ PLUGIN_API VOID OnPulse(VOID)
 			WriteChatf("MQ2AutoCamp :: \arI am dead. Camping to desktop NOW.\ax");
 			PopUpWarning(". . . . Camping to desktop NOW . . . . ");
 			if (bEndMacro && gRunning) DoCommand(GetCharInfo()->pSpawn,"/endmac");
-			DoCommand(GetCharInfo()->pSpawn,"/camp desktop");			
+			DoCommand(GetCharInfo()->pSpawn,"/camp desktop");
 		}
 
 		sprintf_s(szTemp,"${Me.Buff[Resurrection Sickness].ID}");
